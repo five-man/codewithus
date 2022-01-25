45dfdc50af47da8ee1f0967c17990854c5811d19
jjicky <jjlicky@daum.net>
2022-01-25 14:01:56 +0900

    로그인 비밀번호 찾기, 자동로그인 제외 기능 구현, 세션리턴 확인, db연결확인

--- member/views.py
+++ member/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Member
 from django.utils import timezone
 from django.http import HttpResponse
+from django.contrib import messages
 
 # Create your views here.
 def main(request):
@@ -9,8 +10,21 @@ def main(request):
     return render(request,"member/main.html")
 
 def login(request):
+    if request.method == 'POST':
+        member_email = request.POST.get('member_email')
+        member_password = request.POST.get('member_password')
+        try:
+            m = Member.objects.get(member_email=member_email, member_password=member_password)
+        except Member.DoesNotExist as e:
+            messages.info(request, '아이디 혹은 비밀번호를 확인해주세요.')
+            return render(request, 'member/login.html')
+        else:
+            request.session['member_email'] = m.member_email
+            request.session['member_name'] = m.member_name
+        return redirect('/main/')
+    else:
+        return render(request, 'member/login.html')
 
-    return render(request,"member/login.html")
 
 def signup(request):
     if request.method == 'POST':
@@ -23,7 +37,6 @@ def signup(request):
         m.member_joindate = timezone.now()
         m.save()
 
-        return HttpResponse(
-            '가입 완료<br>%s %s %s' % (member_email, member_password, member_name))
+        return redirect('/login/')
     else:
         return render(request, 'member/signup.html')
