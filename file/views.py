d2fdc8bf6ddea055ef1a708f1bb157d30c02e049
swwho96 <swwho96@gmail.com>
2022-01-26 19:36:16 +0900

    파일리스트 구현

--- file/views.py
+++ file/views.py
@@ -1,25 +1,25 @@
+from distutils import filelist
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
-from .models import File
+from .models import File, FileList
 from django.utils import timezone
 from member.models import Member
 
 # Create your views here.
 
-def file(request):
-    return render(request, 'file/file_list.html')
+# def file(request):
+#     return render(request, 'file/file_list.html')
 
 def uploadFile(request):
     if request.method == "POST":
         uploadedFile = request.FILES.get("file")
         must = Member.objects.get(member_no=1)
         name = uploadedFile.name
-        
         file = File(
-            file_name = name, 
+            file_name = uploadedFile.name,
             file_date = timezone.now(),
-            file_volume = 0,
+            file_volume = uploadedFile.size,
             member_no = must
         )
         file.save()
@@ -27,6 +27,6 @@ def uploadFile(request):
         with open(name, 'wb') as file: # 파일 저장
             for chunk in uploadedFile.chunks():
                 file.write(chunk)
-
-    documents = File.objects.all()
-    return render(request, "file/file_list.html", {"files": documents})
+    must = Member.objects.get(member_no=1)
+    filelist = FileList.objects.all()
+    return render(request, "file/file_list.html", {"filelist": filelist})
