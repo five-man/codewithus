0df798204615efbf8be4de0cfc5d77101115f5f2
swwho96 <swwho96@gmail.com>
2022-01-27 21:59:07 +0900

    댓글 출력

--- solve/urls.py
+++ solve/urls.py
@@ -6,6 +6,7 @@ app_name = 'solve'
 urlpatterns = [
     path('problem_list/', views.problem_list, name='problem_list'),    
     path('problem_upload/', views.problem_upload, name='problem_upload'),    
-    path('today_exam/', views.today_exam, name='today_exam'),
-    path('solutions/', views.solutions, name='solutions'),
+    path('today_exam/', views.today_exam, name='today_exam'),    
+    path('solutions/', views.solutions, name='solutions'),    
+    path('reply/', views.reply, name='reply'),    
 ]
--- solve/views.py
+++ solve/views.py
@@ -1,54 +1,111 @@
-from datetime import timezone
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import redirect, render, get_object_or_404
-from django.http import HttpResponse
+import datetime
 import json
-from django.core import serializers
-from django.core.serializers.json import DjangoJSONEncoder
-from solve.models import Algorithm, Comment, Solution
+from django.utils import timezone
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import redirect, render
+from numpy import save
+import os
+from config.settings import BASE_DIR, MEDIA_ROOT
+from solve.models import Algorithm, AlgorithmImage, Comment, Solution
+from member.models import Member
+from django.db.models import Count
 
 # Create your views here.
+
+
 def problem_list(request):
 
-    return render(request,"solve/problem_list.html")
+    return render(request, "solve/problem_list.html")
+
 
 def problem_upload(request):
-    
+
     #
     #   오늘의 문제가 있다면 업로드 못하게 막는 로직
     #
-    
+
     if request.method == 'POST':
         algo_title = request.POST.get('subject')
         algo_detail = request.POST.get('contents')
-        member_no = request.session['member_no']
-        
+        member_no = request.session.get('member_no')
+        uploadedFile = request.FILES.getlist("image")
+
         a = Algorithm(
-            algo_title=algo_title, algo_detail=algo_detail, member_no=member_no)
-        a.algo_update = timezone.now()
+            algo_title=algo_title,
+            algo_detail=algo_detail,
+            member_no=Member.objects.get(member_no=member_no),
+            tag_id=3,
+            algo_update=timezone.now())
         a.save()
 
-        # 글이 써지면 오늘의 문제로 
+        for uploadFile in uploadedFile:
+            # image_name =
+            i = AlgorithmImage(
+                image_name=uploadFile.name,
+                image_root="/media/",
+                algo_no=Algorithm.objects.get(algo_no=87))
+            i.save()
+            save_path = os.path.join(MEDIA_ROOT, i.image_name)
+            with open(save_path, 'wb') as file:
+                for chunk in uploadFile.chunks():
+                    file.write(chunk)
+
+        # 글이 써지면 오늘의 문제로
         return redirect('/today_exam/')
     else:
-        # 
+        #
         return render(request, 'solve/problem_upload.html')
 
     # return render(request,"solve/problem_upload.html")
 
+
 def today_exam(request):
 
-    return render(request,"solve/today_exam.html")
+    # now = datetime.datetime.now()
+    # nowDate = now.strftime('%Y-%m-%d')
+
+    # today_algo = Algorithm.objects.get(algo_no = 87)
+    # algo_image_object = AlgorithmImage.objects.filter(algo_no=today_algo)
+
+    # return render(request, 'solve/today_exam.html', {'image':algo_image_object.image_name})
+
+    return render(request, "solve/today_exam.html")
 
 
-# def solutions(request):
-#     return render(request, "solve/solutions.html")
+def solutions(request, sol_no=50):
+    solution = Solution.objects.get(sol_no=sol_no)
 
+    reply = Comment.objects.filter(sol_no=sol_no)
 
-def solutions(request):
-    com = Comment(
-        sol_no = Solution.objects.get(sol_no = 50),
-        algo_no = Algorithm.objects.get(algo_no = 50)
-     )
-    com.save()
-    return HttpResponse('success')
+    try:
+        session = request.session.get('member_no')
+        context = {
+            'solution': solution,
+            'reply': reply,
+            'session': session,
+        }
+        return render(request, 'solve/solutions.html', context)
+    except KeyError:
+        return redirect('member/main')
+
+from django.core import serializers
+
+def reply(request):
+    member_no = request.session.get('member_no')
+    jsonObject = json.loads(request.body)
+    solno =jsonObject.get('sol_no')
+    reply = Comment.objects.create(
+        sol_no=Solution.objects.get(sol_no=solno),
+        algo_no=Solution.objects.get(algo_no=50),
+        member_no=Member.objects.get(member_no=member_no),
+        comment_detail=jsonObject.get('comment_detail'),
+    )
+    reply.save()
+    membername = Member.objects.get(member_no = member_no)
+    context = {
+        # 'name': serializers.serialize("json", reply.member_no),
+        #'content': reply.comment_detail,
+        'pp': membername.member_name    
+    }
+
+    return JsonResponse(context)
