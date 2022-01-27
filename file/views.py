2a1e7244c7d954ae1b62bd92b3e4ad9ab4e0a18d
swwho96 <swwho96@gmail.com>
2022-01-27 17:03:59 +0900

    파일 삭제 기능

--- file/views.py
+++ file/views.py
@@ -2,14 +2,16 @@ from distutils import filelist
 from importlib.resources import path
 from django.forms import FilePathField
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, response
 from django.core.files.storage import FileSystemStorage
+from sqlalchemy import null
 from .models import File, FileList
 from django.utils import timezone
 from member.models import Member
 import os
 from config import settings
 from config.settings import MEDIA_ROOT
+from django.contrib import messages
 
 # Create your views here.
 
@@ -23,11 +25,19 @@ def uploadFile(request):
         now_member = request.session['member_no']
         must = Member.objects.get(member_no = now_member)
         name = uploadedFile.name
+        file_root = os.path.join(MEDIA_ROOT)
+        
+        try:
+            new_name = FileList.objects.get(file_name = name)
+            new_name = str("new") + str(new_name.file_name)
+        except FileList.DoesNotExist:
+            new_name = name
+
         file = File(
             file_name = fileTitle,
             file_date = timezone.now(),
             file_volume = uploadedFile.size,
-            file_root = os.path.join(MEDIA_ROOT, name),
+            file_root = file_root,
             member_no = must
         )
         file.save()
@@ -35,15 +45,20 @@ def uploadFile(request):
         fileno = File.objects.latest('file_no')
         fileroot = File.objects.latest('file_root')
         filelist = FileList(
-            file_name = name,
+            file_name = new_name,
             file_no = fileno,
-            file_root = fileroot
+            file_root = fileroot.file_root
         )
-        filelist.save()
 
-        with open(name, 'wb') as file: # 파일 저장
-            for chunk in uploadedFile.chunks():
-                file.write(chunk)
+        try:
+            with open(file_root+"wer/"+name, 'wb') as piz: # 파일 저장
+                for chunk in uploadedFile.chunks():
+                    piz.write(chunk)
+        except FileNotFoundError:
+            file.delete()
+            messages.info(request, '정보를 나타냅니다.')
+
+        filelist.save()
     filelist = FileList.objects.all()
     return render(request, "file/file_list.html", {"filelist": filelist})
 
@@ -52,7 +67,7 @@ def download(request):
     if request.method == 'POST':
         fn = request.POST["filename"]
         filename = FileList.objects.get(file_name = fn)
-        filepath = str(settings.BASE_DIR) + ('/%s' % filename.file_name)
+        filepath = str(settings.BASE_DIR) + ('\\media\\%s' % filename.file_name)
         with open(filepath, 'rb') as f:
             response = HttpResponse(f, content_type='application/octet-stream')
             response['Content-Disposition'] = 'attachment; filename=%s' % fn
