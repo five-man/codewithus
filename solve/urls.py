e32fd6b4e8ef5bd9bcdb6042a97802e783ecdb90
swwho96 <swwho96@gmail.com>
2022-02-02 15:14:08 +0900

    nav css 수정

--- solve/urls.py
+++ solve/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     #path('update_exam<int:algo_no><int:sol_no>/', views.update_exam, name='update_exam'),
     #path('algo/', views.algo, name = 'algo'),
     #path('print/', views.print_today),
-    #path('problem_list/exam<int:algo_no>/', views.exam, name='exam'),
+    path('problem_list/exam<int:algo_no>/', views.exam, name='exam'),
     path('problem_upload/complete', views.problem_upload_complete, name='problem_upload_complete'),    
     # path('problem_upload/fail', views.problem_upload_fail, name='problem_upload_fail'),    
     path('today_exam/', views.today_exam, name='today_exam'),    
